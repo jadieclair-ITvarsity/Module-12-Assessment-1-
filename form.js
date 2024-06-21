a868a7eff864fabde5c2fe83cd84cfd07a3487ed
jadeharris <jadieclair@yahoo.com>
2024-06-21 16:42:45 +0200

    Create add contact

---
+++ form.js
@@ -0,0 +1,30 @@
+document.getElementById("submitForm").addEventListener("click", submitForm);
+document.getElementById("homeLink").addEventListener("click", homeLink);
+
+function submitForm(e) {
+  e.preventDefault();
+  const form = new FormData(document.querySelector("#editForm"));
+  form.append("apiKey", apiKey);
+
+  fetch(rootPath + "controller/insert-contact/", {
+    method: "POST",
+    headers: { Accept: "application/json, *.*" },
+    body: form,
+  })
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (data) {
+      if (data === "1") {
+        alert("Contact Added!");
+        homeLink();
+      } else {
+        alert(data);
+        homeLink();
+      }
+    });
+}
+
+function homeLink() {
+  window.open("index.html", "_self");
+}
